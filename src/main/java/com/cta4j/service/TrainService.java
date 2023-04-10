ff8aec9606d1d10b4b1157af12a75e17b6f40617
Logan Kulinski <rashes_lineage02@icloud.com>
2023-04-10 09:55:57 -0500

    GraphQL Experiments

--- src/main/java/com/cta4j/service/TrainService.java
+++ src/main/java/com/cta4j/service/TrainService.java
@@ -1,91 +1,71 @@
 package com.cta4j.service;
 
+import com.cta4j.exception.TrainException;
+import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Service;
-import com.cta4j.client.StationClient;
 import com.cta4j.client.TrainClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Objects;
 import org.springframework.http.ResponseEntity;
-import java.util.Set;
-import com.cta4j.model.Station;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.HttpStatus;
+import java.util.List;
 import com.cta4j.model.Train;
 import com.cta4j.model.TrainResponse;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import com.cta4j.model.TrainBody;
 
 @Service
 public final class TrainService {
-    private final StationClient stationClient;
-
-    private final TrainClient trainClient;
+    private final TrainClient client;
 
     @Autowired
-    public TrainService(StationClient stationClient, TrainClient trainClient) {
-        Objects.requireNonNull(stationClient);
+    public TrainService(TrainClient client) {
+        Objects.requireNonNull(client);
 
-        Objects.requireNonNull(trainClient);
-
-        this.stationClient = stationClient;
-
-        this.trainClient = trainClient;
+        this.client = client;
     }
 
-    public ResponseEntity<Set<Station>> readStations() {
-        ResponseEntity<Set<Station>> responseEntity = this.stationClient.getStations();
-
-        HttpStatusCode statusCode = responseEntity.getStatusCode();
-
-        if ((statusCode != HttpStatus.OK) || !responseEntity.hasBody()) {
-            return ResponseEntity.internalServerError()
-                                 .build();
-        }
-
-        Set<Station> stations = responseEntity.getBody();
-
-        if (stations == null) {
-            return ResponseEntity.internalServerError()
-                                 .build();
-        }
-
-        return ResponseEntity.ok(stations);
-    }
-
-    public ResponseEntity<Set<Train>> readTrains(int stationId) {
+    public List<Train> readTrains(int stationId) {
         if (stationId <= 0) {
-            return ResponseEntity.badRequest()
-                                 .build();
+            String message = "The specified station ID must be positive";
+
+            throw new TrainException(message, ErrorType.BAD_REQUEST);
         }
 
-        ResponseEntity<TrainResponse> responseEntity = this.trainClient.getTrains(stationId);
+        ResponseEntity<TrainResponse> responseEntity = this.client.getTrains(stationId);
 
         HttpStatusCode statusCode = responseEntity.getStatusCode();
 
         if ((statusCode != HttpStatus.OK) || !responseEntity.hasBody()) {
-            return ResponseEntity.internalServerError()
-                                 .build();
+            String message = "The response from the CTA's train API is malformed";
+
+            throw new TrainException(message, ErrorType.INTERNAL_ERROR);
         }
 
         TrainResponse response = responseEntity.getBody();
 
         if (response == null) {
-            return ResponseEntity.internalServerError()
-                                 .build();
+            String message = "The response from the CTA's train API is malformed";
+
+            throw new TrainException(message, ErrorType.INTERNAL_ERROR);
         }
 
         TrainBody body = response.body();
 
         if (body == null) {
-            return ResponseEntity.internalServerError()
-                                 .build();
+            String message = "The response from the CTA's train API is malformed";
+
+            throw new TrainException(message, ErrorType.INTERNAL_ERROR);
         }
 
-        Set<Train> trains = body.trains();
+        List<Train> trains = body.trains();
 
         if (trains == null) {
-            trains = Set.of();
+            String message = "The response from the CTA's train API is malformed";
+
+            throw new TrainException(message, ErrorType.INTERNAL_ERROR);
         }
 
-        return ResponseEntity.ok(trains);
+        return trains;
     }
 }
