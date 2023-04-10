9e6843e670a6960530a4fe68fd0b94a49ae38d33
Logan Kulinski <rashes_lineage02@icloud.com>
2023-04-10 11:43:23 -0500

    Added support for requesting CTA train stations

--- src/main/java/com/cta4j/config/HttpClientConfiguration.java
+++ src/main/java/com/cta4j/config/HttpClientConfiguration.java
@@ -2,15 +2,30 @@ package com.cta4j.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
-import com.cta4j.client.TrainClient;
-import org.springframework.beans.factory.annotation.Value;
-import java.util.Objects;
+import com.cta4j.client.StationClient;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import com.cta4j.client.TrainClient;
+import org.springframework.beans.factory.annotation.Value;
+import java.util.Objects;
 
 @Configuration
 public class HttpClientConfiguration {
+    @Bean
+    public StationClient stationClient() {
+        String baseUrl = "https://data.cityofchicago.org/resource/8pix-ypme.json";
+
+        WebClient webClient = WebClient.create(baseUrl);
+
+        WebClientAdapter webClientAdapter = WebClientAdapter.forClient(webClient);
+
+        HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builder(webClientAdapter)
+                                                                                 .build();
+
+        return httpServiceProxyFactory.createClient(StationClient.class);
+    }
+
     @Bean
     public TrainClient trainClient(@Value("${TRAIN_API_KEY}") String apiKey) {
         Objects.requireNonNull(apiKey);
