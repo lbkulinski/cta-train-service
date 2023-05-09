c23269de890751ed6abf5897b63e78484477bcb7
Logan Kulinski <rashes_lineage02@icloud.com>
2023-05-09 17:55:02 -0500

    Validation exception handling

--- src/main/java/com/cta4j/config/CustomExceptionResolver.java
+++ src/main/java/com/cta4j/config/CustomExceptionResolver.java
@@ -10,31 +10,48 @@ import graphql.ErrorClassification;
 import graphql.execution.ResultPath;
 import graphql.language.SourceLocation;
 import graphql.GraphqlErrorBuilder;
+import org.springframework.validation.BindException;
+import org.springframework.graphql.execution.ErrorType;
 
 @Component
 public final class CustomExceptionResolver extends DataFetcherExceptionResolverAdapter {
     @Override
     protected GraphQLError resolveToSingleError(@NonNull Throwable throwable,
         @NonNull DataFetchingEnvironment environment) {
-        if (!(throwable instanceof DataFetcherException exception)) {
-            return null;
+        if (throwable instanceof DataFetcherException exception) {
+            ErrorClassification errorType = exception.getErrorType();
+
+            String message = throwable.getMessage();
+
+            ResultPath path = environment.getExecutionStepInfo()
+                                         .getPath();
+
+            SourceLocation location = environment.getField()
+                                                 .getSourceLocation();
+
+            return GraphqlErrorBuilder.newError()
+                                      .errorType(errorType)
+                                      .message(message)
+                                      .path(path)
+                                      .location(location)
+                                      .build();
+        } else if (throwable instanceof BindException) {
+            String message = "The specified station ID must be positive integer";
+
+            ResultPath path = environment.getExecutionStepInfo()
+                                         .getPath();
+
+            SourceLocation location = environment.getField()
+                                                 .getSourceLocation();
+
+            return GraphqlErrorBuilder.newError()
+                                      .errorType(ErrorType.BAD_REQUEST)
+                                      .message(message)
+                                      .path(path)
+                                      .location(location)
+                                      .build();
         }
 
-        ErrorClassification errorType = exception.getErrorType();
-
-        String message = throwable.getMessage();
-
-        ResultPath path = environment.getExecutionStepInfo()
-                                     .getPath();
-
-        SourceLocation location = environment.getField()
-                                             .getSourceLocation();
-
-        return GraphqlErrorBuilder.newError()
-                                  .errorType(errorType)
-                                  .message(message)
-                                  .path(path)
-                                  .location(location)
-                                  .build();
+        return null;
     }
 }
